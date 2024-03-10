3ab5063ddfd159cd9e38e72dc90147bd3a6ae47e
JasonMun7 <jasonmun020104@gmail.com>
2024-03-10 06:10:58 -0400

    added fetch for posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
-"use client"
-
 import Image from "next/image";
 import { useState, useEffect } from 'react'
 import React from "react";
-import { CardBody, CardContainer, CardItem } from '../components/3d-card'
+import Navbar from "../components/navbar"
+import background1 from '../assets/background1.png'
+import {supabaseBrowser} from '../lib/supabase/browser'
+import {createClient} from '../lib/supabase/server'
 
-export default function Home() {
+
+const supabase = createClient()
+
+async function fetchData(){
+  const {data, error} = await supabase.from("Posts").select()
+  if (error){
+    throw error
+  }
+  return data
+}
+
+export default async function Home() {
+  const post = await fetchData()
+  console.log(post)
+
   return (
     <>
+      <div className="bg-cover bg-center h-[100vh]" style={{ backgroundImage: `url(${background1.src || background1})`}}>
+        <Navbar/>
+      </div>
     </>
   );
 }
