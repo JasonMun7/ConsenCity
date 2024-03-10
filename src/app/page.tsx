0ed9e51001ca7b571fc74039b7ffd3368421653c
JasonMun7 <jasonmun020104@gmail.com>
2024-03-09 21:04:21 -0500

    Updated layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { CardBody, CardContainer, CardItem } from '../components/3d-card'
 export default function Home() {
   return (
     <>
+    <div className="sm:w-72 w-54">Hello world</div>
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
         <CardItem
