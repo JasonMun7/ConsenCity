703c556d1b46795fb6728175071ee620f1db5aba
JasonMun7 <jasonmun020104@gmail.com>
2024-03-09 23:24:33 -0500

    Added Daisy Ui and added the landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,17 +16,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [addVariablesForColors],
+  plugins: [require("daisyui")],
 };
 
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }: any) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
