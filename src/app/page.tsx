403e6cb4be0f9eba2005e80b46d2d8c6bc15a986
Dora <cyweng03@gmail.com>
2024-03-10 06:01:49 -0400

    changed some things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { useState, useEffect } from 'react'
 import React from "react";
-import { CardBody, CardContainer, CardItem } from '../components/3d-card'
 
 export default function Home() {
   return (
